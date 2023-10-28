998af450dd8e69bfd0a46d37bbe258cbcdd31202
Praveen Kumar <praveenscience@gmail.com>
2023-10-28 15:25:57 +0100

    Code Cleanup from CRA.

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello Pesto</h1>
+        <p>Hello everyone!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
